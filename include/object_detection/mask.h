012c2d9f655e4765677997575bd69e35c18cc76f
Stephan Wirth <stephan.wirth@uib.es>
2011-09-20 15:12:01 +0200

    formatting

--- include/object_detection/mask.h
+++ include/object_detection/mask.h
@@ -15,15 +15,15 @@ namespace object_detection {
  */
 struct Mask 
 {
-    /// the rectangle that defines the position of the image mask
-    cv::Rect rect;
+  /// the rectangle that defines the position of the image mask
+  cv::Rect rect;
 
-    /// the image mask that masks an object of interest.
-    /// a value above zero means that the pixel belongs to the object.
-    /// As a convention if the image has zero size, the whole rect
-    /// masks the object (same as if all values in the image are
-    /// above zero).
-    cv::Mat image;
+  /// the image mask that masks an object of interest.
+  /// a value above zero means that the pixel belongs to the object.
+  /// As a convention if the image has zero size, the whole rect
+  /// masks the object (same as if all values in the image are
+  /// above zero).
+  cv::Mat image;
 };
 
 }

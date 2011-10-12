5670a93d1f5bbcc7a51504818916dda56b91d9f2
Stephan Wirth <stephan.wirth@uib.es>
2011-10-12 09:44:22 +0200

    added new param for mean filter size

--- src/object_detection_ros/detector_nodelet.cpp
+++ src/object_detection_ros/detector_nodelet.cpp
@@ -25,6 +25,8 @@ namespace object_detection_ros {
 
       ros::Publisher detections_pub_;
 
+      ros::Subscriber training_data_sub_;
+
     public:
       virtual void onInit()
       {
@@ -70,7 +72,7 @@ namespace object_detection_ros {
       void loadSettings(ros::NodeHandle& nh)
       {
         // color detector settings
-        int num_hue_bins, num_saturation_bins, min_saturation, min_value, morph_element_size;
+        int num_hue_bins, num_saturation_bins, min_saturation, min_value, morph_element_size, mean_filter_size;
         bool show_images;
         if (!nh.hasParam("num_hue_bins")) NODELET_ERROR("Param num_hue_bins not found!");
         if (nh.getParam("num_hue_bins", num_hue_bins)) color_detector_->setNumHueBins(num_hue_bins);
@@ -78,6 +80,7 @@ namespace object_detection_ros {
         if (nh.getParam("min_saturation", min_saturation)) color_detector_->setMinSaturation(min_saturation);
         if (nh.getParam("min_value", min_value)) color_detector_->setMinValue(min_value);
         if (nh.getParam("morph_element_size", morph_element_size)) color_detector_->setMorphElementSize(morph_element_size);
+        if (nh.getParam("mean_filter_size", mean_filter_size)) color_detector_->setMeanFilterSize(mean_filter_size);
         if (nh.getParam("show_images", show_images)) color_detector_->setShowImages(show_images);
 
         // shape detector settings
@@ -95,12 +98,14 @@ namespace object_detection_ros {
                      "  Minimum Saturation       : %i \n"
                      "  Minimum Value            : %i \n"
                      "  Morph Element Size       : %i \n"
+                     "  Mean Filter Size         : %i \n"
                      "  Show Images              : %s \n",
                      color_detector_->numHueBins(),
                      color_detector_->numSaturationBins(),
                      color_detector_->minSaturation(),
                      color_detector_->minValue(),
                      color_detector_->morphElementSize(),
+                     color_detector_->meanFilterSize(),
                      (color_detector_->showImages() ? "true" : "false"));
         NODELET_INFO("Current ShapeDetector settings:\n"
                      "  Matching Score Threshold : %f \n"
@@ -126,6 +131,8 @@ namespace object_detection_ros {
       {
         it_.reset(new image_transport::ImageTransport(nh));
         image_sub_ = it_->subscribe("image", 1, &DetectorNodelet::detectionCb, this);
+
+        training_data_sub_ = nh.subscribe("training_data", 1, &DetectorNodelet::trainingDataCb, this);
       }
 
       void detectionCb(const sensor_msgs::ImageConstPtr& image_msg)
@@ -163,13 +170,43 @@ namespace object_detection_ros {
         }
       }
 
-      /*
+      
       void trainingDataCb(const vision_msgs::TrainingDataConstPtr& training_data_msg)
       {
+        NODELET_INFO("Training data received, running training.");
         odat::TrainingData training_data;
-        odat_ros::fromMsg(training_data_msg, training_data);
+        odat_ros::fromMsg(*training_data_msg, training_data);
+        color_detector_->startTraining("target");
+        color_detector_->trainInstance("target", training_data);
+        color_detector_->endTraining("target");
+        // run detection with color detector to get training data for shape detector
+        color_detector_->setImage(training_data.image);
+        color_detector_->detect();
+        std::vector<odat::Detection> color_detections = color_detector_->getDetections();
+        if (color_detections.size() == 0)
+        {
+          NODELET_ERROR("Training error, color detector did not detect anything after training. Object cannot be trained.");
+          return;
+        }
+        // select detection of target
+        int index = -1;
+        for (size_t i = 0; i < color_detections.size(); ++i)
+        {
+          if (color_detections[i].label == "target")
+            index = i;
+        }
+        if (index < 0)
+        {
+          NODELET_ERROR("Training error, color detector did not detect the target after training. Object cannot be trained.");
+          return;
+        }
+        training_data.mask = color_detections[index].mask;
+        shape_detector_->startTraining("target");
+        shape_detector_->trainInstance("target", training_data);
+        shape_detector_->endTraining("target");
+        NODELET_INFO("Target trained.");
       }
-      */
+     
   };
 
 }
